2086a1e22b0b77f8fa6d62f7f73f19a66692bb8b
HAMRAS MUHAMMAD <67454866+ham-ras@users.noreply.github.com>
2022-02-03 20:10:11 +0530

    Update Exc 28_class.py

--- Exc 28_class.py
+++ Exc 28_class.py
@@ -8,20 +8,29 @@ class Rectangle:
     def perimeter(self):
         self.p=2*(self.l+self.b)
         return(self.p)
+    def compare(self,rectangle):
+        if self.a == rectangle.a:
+            print("both are equal")
+        else:
+            print("both are not equal")
 
-rect1=Rectangle(3,4)
-rect2=Rectangle(4,5)
+
+l1=int(input("Enter the length rectangle 1 : "))
+b1=int(input("Enter the breadth rectangle 1 : "))
+rect1=Rectangle(l1,b1)
 rect1.area()
 peri=rect1.perimeter()
 print("Perimeter : ",peri)
 print("")
+
+l2=int(input("Enter the length rectangle 2 : "))
+b2=int(input("Enter the breadth rectangle 2 : "))
+rect2=Rectangle(l2,b2)
 rect2.area()
 peri=rect2.perimeter()
 print("Perimeter : ",peri)
 
-if rect1.a == rect2.a:
-    print("both are equal")
-else:
-    print("both are not equal")
+rect1.compare(rect2)
+
 
     
